c009a3d460ceb939c59fc7c7e9481d4fd66b1df3
Prashant Varanasi <prashant@chronosphere.io>
2024-05-17 13:40:07 -0700

    Deprecate ProxyURL in HTTP notifiers
    See sc-91795 for details.

--- chronosphere/tfschema/webhook_alert_notifier.go
+++ chronosphere/tfschema/webhook_alert_notifier.go
@@ -54,8 +54,9 @@ var WebhookAlertNotifier = map[string]*schema.Schema{
 		ConflictsWith: []string{"basic_auth_username"},
 	},
 	"proxy_url": {
-		Type:     schema.TypeString,
-		Optional: true,
+		Type:       schema.TypeString,
+		Optional:   true,
+		Deprecated: "custom proxy URLs are not supported",
 	},
 	"tls_insecure_skip_verify": {
 		Type:     schema.TypeBool,
