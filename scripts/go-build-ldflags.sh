58039c5a41598b506b5bb47ac0ab160ef8ecdb13
Adam Schepis (he/him) <adam.schepis@chronosphere.io>
2024-10-31 11:47:47 -0400

    Fix snapshot builds to have correct version (#89)
    Co-authored-by: Brandon Gonzalez <bg@chronosphere.io>

--- scripts/go-build-ldflags.sh
+++ scripts/go-build-ldflags.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export GIT_VERSION=$(git describe --tags --abbrev=0 2>/dev/null || echo unknown)
 export GIT_REVISION=$(git rev-parse --short HEAD)
 export BUILD_DATE=$(date '+%F-%T') # outputs something in this format 2017-08-21-18:58:45
 export BASE_PACKAGE=github.com/chronosphereio/terraform-provider-chronosphere/chronosphere/buildinfo
