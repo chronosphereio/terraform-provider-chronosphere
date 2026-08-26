b0e4ed6afbb0c0a21960982fed8af716c877dbfa
Ryan Allen <rallen090@gmail.com>
2026-08-26 15:24:58 -0500

    Sync swagger to the current public API spec
    Re-syncs the config swagger specs. Picks up the new unstable
    QueryResourcePools API (CRUD operations and models for pool-based
    datapoints-read limiting of automated query sources), along with
    unrelated configv1 drift in the synthetic-test auth models.

--- chronosphere/pkg/configv1/models/configv1_o_auth2_resource_owner_password.go
+++ chronosphere/pkg/configv1/models/configv1_o_auth2_resource_owner_password.go
@@ -13,26 +13,26 @@ import (
 	"github.com/go-openapi/swag"
 )
 
-// Configv1OAuth2ResourceOwnerPassword OAuth2ResourceOwnerPassword configures the OAuth 2.0 resource-owner
-// password-credentials grant. client_id/client_secret are optional, set when
-// the token endpoint also authenticates the client.
+// Configv1OAuth2ResourceOwnerPassword Configures the OAuth 2.0 resource-owner password-credentials grant. Set
+// `client_id` and `client_secret` when the token endpoint also authenticates
+// the client.
 //
 // swagger:model configv1OAuth2ResourceOwnerPassword
 type Configv1OAuth2ResourceOwnerPassword struct {
 
-	// client id
+	// Client identifier issued to the test.
 	ClientID string `json:"client_id,omitempty"`
 
-	// client secret
+	// Client secret issued to the test.
 	ClientSecret string `json:"client_secret,omitempty"`
 
 	// common
 	Common *Configv1OAuth2Common `json:"common,omitempty"`
 
-	// password
+	// Password of the resource owner.
 	Password string `json:"password,omitempty"`
 
-	// username
+	// User name of the resource owner.
 	Username string `json:"username,omitempty"`
 }
 
